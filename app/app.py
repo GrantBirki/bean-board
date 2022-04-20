5e82b5bfbcbbc763e16861252170e818b0727215
Grant Birkinbine <grant.birkinbine@gmail.com>
2022-04-19 21:44:51 -0400

    add success page

--- app/app.py
+++ app/app.py
@@ -21,7 +21,6 @@ def health_check():
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
 @app.route("/", methods=["GET", "POST"])
 def upload_file():
     if request.method == "POST":
@@ -38,7 +37,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-            return "uploaded!"
+            return render_template("success.html")
     else:
         return render_template("index.html")
 
